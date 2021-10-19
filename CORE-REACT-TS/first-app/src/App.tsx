807b17b879f714035efdd54e1270b4641651fd7a
Subham Kumar Shaw <subhamshaw16797@gmail.com>
2021-10-19 23:25:44 +0530

    React

--- CORE-REACT-TS/first-app/src/App.tsx
+++ CORE-REACT-TS/first-app/src/App.tsx
@@ -1,15 +1,6 @@
 import React from "react";
 import './App.css';
-import NavBar from "./Component/Layout/NavBar";
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Home from "./Component/Layout/Home";
-import About from './Component/Layout/About';
-import Employees from './Component/Employees/Employees';
-import Stocks from './Component/Stocks/Stocks';
-import UserRegister from './Component/Users/UserRegister';
-import UserLogin from './Component/Users/UserLogin';
-import EmployeeDetails from "./Component/Employees/EmployeeDetails";
-import StocksDetails from './Component/Stocks/StocksDetails';
+import GithubSearch from "./Component/GithubSearch";
 
 interface IProps
 {
@@ -30,20 +21,10 @@ class App extends React.Component<IProps,IState>
     {
         return(
         <React.Fragment>
-            <Router>
-                <NavBar/>
-                {/* <Employees/> */}
-                <Switch>
-                    <Route exact path='/' component={Home}/>
-                    <Route exact path='/about' component={About}/>
-                    <Route exact path='/employees/list' component={Employees}/>
-                    <Route exact path='/employees/:employeeId' component={EmployeeDetails}/>
-                    <Route exact path='/stocks/list' component={Stocks}/>
-                    <Route exact path='/stocks/:stockId' component={StocksDetails}/>
-                    <Route exact path='/users/register' component={UserRegister}/>
-                    <Route exact path='/users/login' component={UserLogin}/>
-                </Switch>
-            </Router>
+            <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
+                <a href="abc" className="navbar-brand">React Github Profile Search App</a>
+            </nav>
+            <GithubSearch/>
         </React.Fragment>
         )
     }
